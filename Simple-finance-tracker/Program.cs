b928002d30d2ceb2be1507b00ccf860d0570db18
Daniel Nikolaj Hartwich <73463118+Drenzy@users.noreply.github.com>
2023-12-13 14:23:01 +0100

    Added SQLite integration for persistent storage, allowing the finance tracker to save and load transactions from an embedded database. Implemented a 'Clear History and Balance' option in the user interface, providing the ability to reset both the transaction history and balance. Enhanced user input handling for better error management using int.TryParse and decimal.TryParse. The console now clears after displaying transactions and when clearing history and balance. Updated the main program and FinanceTracker class to incorporate these improvements.

--- Simple-finance-tracker/Program.cs
+++ Simple-finance-tracker/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Data.SQLite;
 
 class Transaction
 {
+    public int Id { get; set; }
     public string Description { get; set; }
     public decimal Amount { get; set; }
     public DateTime Date { get; set; }
@@ -16,10 +16,54 @@ class Transaction
 class FinanceTracker
 {
     private List<Transaction> transactions;
+    private const string ConnectionString = "Data Source=FinanceTracker.db;Version=3;";
 
     public FinanceTracker()
     {
-        transactions = new List<Transaction>();
+        InitializeDatabase();
+        transactions = LoadTransactionsFromDatabase();
+    }
+
+    private void InitializeDatabase()
+    {
+        using (var connection = new SQLiteConnection(ConnectionString))
+        {
+            connection.Open();
+
+            var command = new SQLiteCommand(
+                "CREATE TABLE IF NOT EXISTS Transactions (Id INTEGER PRIMARY KEY AUTOINCREMENT, Description TEXT, Amount REAL, Date DATETIME)",
+                connection);
+
+            command.ExecuteNonQuery();
+        }
+    }
+
+    private List<Transaction> LoadTransactionsFromDatabase()
+    {
+        var transactions = new List<Transaction>();
+
+        using (var connection = new SQLiteConnection(ConnectionString))
+        {
+            connection.Open();
+
+            var command = new SQLiteCommand("SELECT * FROM Transactions", connection);
+
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    transactions.Add(new Transaction
+                    {
+                        Id = Convert.ToInt32(reader["Id"]),
+                        Description = Convert.ToString(reader["Description"]),
+                        Amount = Convert.ToDecimal(reader["Amount"]),
+                        Date = Convert.ToDateTime(reader["Date"])
+                    });
+                }
+            }
+        }
+
+        return transactions;
     }
 
     public void AddTransaction(string description, decimal amount)
@@ -32,7 +76,28 @@ class FinanceTracker
         };
 
         transactions.Add(transaction);
+
+        SaveTransactionToDatabase(transaction);
+
         Console.WriteLine("Transaction added successfully.");
+    }
+
+    private void SaveTransactionToDatabase(Transaction transaction)
+    {
+        using (var connection = new SQLiteConnection(ConnectionString))
+        {
+            connection.Open();
+
+            var command = new SQLiteCommand(
+                "INSERT INTO Transactions (Description, Amount, Date) VALUES (@description, @amount, @date)",
+                connection);
+
+            command.Parameters.AddWithValue("@description", transaction.Description);
+            command.Parameters.AddWithValue("@amount", transaction.Amount);
+            command.Parameters.AddWithValue("@date", transaction.Date);
+
+            command.ExecuteNonQuery();
+        }
     }
 
     public void DisplayTransactions()
@@ -53,6 +118,19 @@ class FinanceTracker
         }
         return balance;
     }
+
+    public void ClearHistoryAndBalance()
+    {
+        transactions.Clear();
+
+        using (var connection = new SQLiteConnection(ConnectionString))
+        {
+            connection.Open();
+
+            var command = new SQLiteCommand("DELETE FROM Transactions", connection);
+            command.ExecuteNonQuery();
+        }
+    }
 }
 
 class Program
@@ -66,33 +144,53 @@ class Program
             Console.WriteLine("1. Add Transaction");
             Console.WriteLine("2. Display Transactions");
             Console.WriteLine("3. Calculate Balance");
-            Console.WriteLine("4. Exit");
+            Console.WriteLine("4. Clear History and Balance");
+            Console.WriteLine("5. Exit");
 
             Console.Write("Enter your choice: ");
-            int choice = int.Parse(Console.ReadLine());
+            string userInput = Console.ReadLine();
 
-            switch (choice)
+            if (int.TryParse(userInput, out int choice))
             {
-                case 1:
-                    Console.Write("Enter description: ");
-                    string description = Console.ReadLine();
-                    Console.Write("Enter amount: ");
-                    decimal amount = decimal.Parse(Console.ReadLine());
-                    financeTracker.AddTransaction(description, amount);
-                    break;
-                case 2:
-                    financeTracker.DisplayTransactions();
-                    break;
-                case 3:
-                    decimal balance = financeTracker.CalculateBalance();
-                    Console.WriteLine($"Current Balance: {balance:C}");
-                    break;
-                case 4:
-                    Environment.Exit(0);
-                    break;
-                default:
-                    Console.WriteLine("Invalid choice. Please try again.");
-                    break;
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter description: ");
+                        string description = Console.ReadLine();
+                        Console.Write("Enter amount: ");
+                        if (decimal.TryParse(Console.ReadLine(), out decimal amount))
+                        {
+                            financeTracker.AddTransaction(description, amount);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid amount. Please enter a valid decimal number.");
+                        }
+                        break;
+                    case 2:
+                        financeTracker.DisplayTransactions();
+                        Console.Clear(); // Clear the console after displaying transactions
+                        break;
+                    case 3:
+                        decimal balance = financeTracker.CalculateBalance();
+                        Console.WriteLine($"Current Balance: {balance:C}");
+                        break;
+                    case 4:
+                        financeTracker.ClearHistoryAndBalance();
+                        Console.WriteLine("Transaction history and balance cleared.");
+                        Console.Clear(); // Clear the console after clearing history and balance
+                        break;
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
             }
         }
     }
